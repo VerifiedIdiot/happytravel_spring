7e78da95bc2f77569c453e24a85bada227c9f46a
SeokJune <op2se1@gmail.com>
2024-06-20 16:39:34 +0900

    delete auth files

--- src/main/java/kr/happytravel/erp/auth/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package kr.happytravel.erp.auth.service;
-
-import kr.happytravel.erp.auth.model.UserModel;
-
-public interface UserService {
-    UserModel getUserByUsername(String username);
-    boolean checkPassword(String rawPassword, String encodedPassword);
-    void registerUser(UserModel user);
-}
